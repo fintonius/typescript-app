b6927908534dcf5ec57dc0dcc28e242b7698e47e
Fintan Ryan <fintonius.mcginty@gmail.com>
2024-06-15 10:06:21 +0100

    add function to remove a task

--- index.ts
+++ index.ts
@@ -1,5 +1,20 @@
 let index = 0;
-let tasks = [];
+
+// This is telling TS that this array will be expected to only be filled
+// with Task types so if we accidentally push any other type into the 
+// array it will throw an error. TS also throws an error if you don't
+// define the type that the array is expected to return so you need to
+// know this when you're creating it!
+// The '<>' used to define the type the array will return is TS syntax
+// called 'generics'. They act kind of like the arguments, '()', that a 
+// function takes. You can use other JS types, 'string', 'number', 'object',
+// etc., in generics but as it's TS being as specific as possible is 
+// always best!
+let tasks: Array<Task> = [];
+// IMPORTANT NOTE: Arrays in TS cannot change the type of items it can take
+// once it's defined - you could not pass a 'number' type into an array 
+// declared as 'Array<String>' for example.
+// Arrays in TS can take mixed types 
 
 // 'Task' is a type alias. It is like a variable in JS and lets you define a
 // type that can be reused throughout your code. Types can also be imported
@@ -31,6 +46,23 @@ function create(content: string): Task {
     return task;
 }
 
-function complete(task: Task) {   
-    task.status = 'complete';
+function complete(id: number) {   
+    let task = tasks.find((task) => task.id  === id);
+    // the 'if' statement is to ensure that 'task' was passed a valid id so that we
+    // can be sure that changing 'task.status' to 'complete' is correct. As the 
+    // value for 'task' when you hover over it is 'undefined' it means TS can't be
+    // sure what the correct value for 'task.id' is so will throw an error to make
+    // sure we are certain the value is correct and not 'undefined'! Hence checking
+    // below that task is true before changing task.status
+    if (task) task.status = 'complete';
+}
+
+function remove(id: number) {
+    let index = tasks.findIndex((task) => task.id === id);
+    // As findIndex returns '-1' when it cannot find a match 
+    // this 'if' statement is to ensure that we are receiving a valid id. 
+    // IT'S IMPORTANT TO NOTE THAT TS ITSELF DOES NOT CATCH THIS BUG AS 
+    // -1 IS STILL A NUMBER SO TS DOESN'T PICKUP ON THE FACT IT'S NOT WHAT
+    // WE WANT! 
+    if (index !== -1) tasks.splice(index, 1);
 }
